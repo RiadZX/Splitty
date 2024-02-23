47d5d104310bb818cfee4039b5863d3764c296c4
Codrin Radeţchi <radetchi@student.tudelft.nl>
2024-02-23 23:16:38 +0100

    Update Expense.java should work now

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -4,9 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.@OneToMany
-import jakarta.persistence.@ManyToOne
-import jakarta.persistence.@Column
+import jakarta.persistence.OneToMany
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Column
 
 import java.time.LocalDateTime;
 import java.util.List;
