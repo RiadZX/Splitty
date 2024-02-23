49e226b88c4009797fc5ec4cfe3c86403fdaffdd
Riad Zaid <62952229+RiadZX@users.noreply.github.com>
2024-02-23 21:38:19 +0100

    Created participant class, to be used within the application

---
+++ commons/src/main/java/commons/Event.java
@@ -0,0 +1,14 @@
+package commons;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
