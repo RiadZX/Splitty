e696d1f5ed21dbd1a81417ecc67c09751e517965
Codrin Radeţchi <radetchi@student.tudelft.nl>
2024-02-23 22:47:48 +0100

    Update Expense.java

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -130,27 +130,3 @@ public class Expense {
         this.date = date;
     }
 }
-=======
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-@Entity
-public class Expense {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    public Expense() {
-    }
-    public Expense(long id) {
-        this.id = id;
-    }
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-
-}
->>>>>>> commons/src/main/java/commons/Expense.java
