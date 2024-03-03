a5e533770bcdc9869932cb090a423e64d28c63c8
Georges Ataya <g.r.ataya@student.tudelft.nl>
2024-03-03 22:38:53 +0100

    Setting important groundwork for the test classes of ParticipantController and ParticipantRepository. Fixed Minor Issue in ParticipantController.java

--- server/src/test/java/server/api/ParticipantControllerTest.java
+++ server/src/test/java/server/api/ParticipantControllerTest.java
@@ -1,5 +1,41 @@
 package server.api;
 
+import commons.Event;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.OK;
+
+import commons.Participant;
+
+import java.util.UUID;
+
 public class ParticipantControllerTest {
+    private TestParticipantRepository repo;
+    private ParticipantController participantController;
+    private Participant testParticipant1;
+    private Participant testParticipant2;
+    private Event testEvent;
+
+    @BeforeEach
+    public void setup() {
+        repo = new TestParticipantRepository();
+        participantController = new ParticipantController(repo);
+        testEvent = new Event("eventA");
+        testParticipant1 = new Participant();
+        testParticipant2 = new Participant("Jason", testEvent, "BE123456789"
+                , "Jason@smith.com", "12472014");
+    }
+
+    @Test
+    public void testAddNull(){
+        var requestResult1 = participantController.add(testParticipant1);
+        var requestResult2 = participantController.add(testParticipant2);
+
+        assertEquals(BAD_REQUEST, requestResult1.getStatusCode());
+        assertEquals(BAD_REQUEST, requestResult2.getStatusCode());
+    }
 
 }
