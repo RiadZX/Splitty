7a512726df6b4548a4f95bb3b0d53ad56c323032
Andrei Ilinescu <a.ilinescu@student.tudelft.nl>
2024-03-07 17:50:10 +0100

    Fixed small issue regarding file path in Config class

--- client/src/main/java/client/utils/Config.java
+++ client/src/main/java/client/utils/Config.java
@@ -1,13 +1,21 @@
 package client.utils;
 
 import java.io.*;
+import java.util.Objects;
 
 public class Config {
     private static  final String USER_FILENAME="userConfig";
 
     public static void writeUserConfigFile(User user){
-        File file=new File("./client/build/resources/main", USER_FILENAME);
         String userDirectory = new File("").getAbsolutePath();
+        String[] sp=userDirectory.split("/");
+        File file;
+        if (Objects.equals(sp[sp.length - 1], "client")) {
+            file = new File("./build/resources/main", USER_FILENAME);
+        }
+        else {
+            file = new File("./client/build/resources/main", USER_FILENAME);
+        }
         System.out.println(userDirectory);
         try {
             if (file.createNewFile()){
