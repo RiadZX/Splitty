f485e8077633927fecc89ff040de157a8a73b704
Tijmen de Jong <t.b.dejong@student.tudelft.nl>
2024-03-07 20:32:19 +0100

    remove unused imports

--- server/src/test/java/server/api/DebtControllerTest.java
+++ server/src/test/java/server/api/DebtControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import server.database.DebtRepository;
 
 public class DebtControllerTest {
 
--- server/src/test/java/server/api/TestDebtRepository.java
+++ server/src/test/java/server/api/TestDebtRepository.java
@@ -1,7 +1,6 @@
 package server.api;
 
 import commons.Debt;
-import commons.Expense;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
