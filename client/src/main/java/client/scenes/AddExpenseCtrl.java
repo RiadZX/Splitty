dccedc2cf7f869c6b076b396b8a7ca1f2b68f068
Codrin Radetchi <Radetchi@student.tudelft.nl>
2024-03-09 15:35:09 +0100

    completed tag functionality

--- client/src/main/java/client/scenes/AddExpenseCtrl.java
+++ client/src/main/java/client/scenes/AddExpenseCtrl.java
@@ -6,6 +6,7 @@ import commons.*;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
@@ -19,21 +20,17 @@ public class AddExpenseCtrl implements Initializable {
     private final MainCtrl mainCtrl;
     private Event event;
     @FXML
-    private CheckBox allBox;
+    private CheckBox allBox, someBox;
     @FXML
-    private CheckBox someBox;
-    @FXML
-    private ComboBox<String> paidBySelector, partialPaySelector, currencySelector;
+    private ComboBox<String> paidBySelector, currencySelector;
     @FXML
     private TextField howMuchField, tagField;
     @FXML
     private DatePicker whenField;
     @FXML
-    private VBox tagSelector;
+    private VBox tagSelector, createTagBox, partialPaidSelector;
     @FXML
-    private VBox createTagBox;
-    @FXML
-    private Label tagErrorLabel;
+    private Label tagErrorLabel, errorLabel;
 
     @Inject
     public AddExpenseCtrl(ServerUtils server, MainCtrl mainCtrl, Event event) {
@@ -52,22 +49,36 @@ public class AddExpenseCtrl implements Initializable {
     @FXML
     public void checkAll(){
         someBox.setSelected(false);
+        partialPaidSelector.setVisible(false);
     }
 
     @FXML
     public void checkSome(){
         allBox.setSelected(false);
+        partialPaidSelector.setVisible(true);
     }
 
     public void setup(Event event){
         this.event = event;
         createTagBox.setVisible(false);
+        System.out.println(event.getParticipants().size());
+        paidBySelector.setItems(FXCollections.observableList(event.getParticipants().stream().map(Participant::getName).toList()));
+        errorLabel.setVisible(false);
+
         currencySelector.setItems(FXCollections.observableList(Stream.of("EUR", "USD", "RON").toList()));
         currencySelector.setValue("EUR");
         currencySelector.setVisible(true);
+
+        tagSelector.getChildren().clear();
         for (int i = 1; i<event.getTags().size() + 1; i++){
             CheckBox checkbox = new CheckBox(event.getTags().get(i-1).getTag());
             tagSelector.getChildren().add(checkbox);
+        }
+
+        partialPaidSelector.setVisible(false);
+        partialPaidSelector.getChildren().clear();
+        for (Participant p : event.getParticipants()){
+            partialPaidSelector.getChildren().add(new CheckBox(p.getName()));
         }
     }
 
@@ -76,22 +87,44 @@ public class AddExpenseCtrl implements Initializable {
         List<Participant> participantList = event.getParticipants();
 
         //store who paid
-        Participant paidBy = null;
-        for (Participant p : participantList){
-            if (p.getName().equals(paidBySelector.getValue())) {
-                paidBy = p;
-            }
-        }
+        Participant paidBy = findParticipant(paidBySelector.getValue());
         if (paidBy == null) {
+            showErrorLabel("Please select who paid!");
+            return;
+        }
+
+        if (!someBox.isSelected() && !allBox.isSelected()){
+            showErrorLabel("Please select how to split!");
             return;
         }
 
         //create a list of debtors
         participantList.remove(paidBy);
+        if (someBox.isSelected()){
+            for(Node c : partialPaidSelector.getChildren()){
+                if (c.getClass() == CheckBox.class && !((CheckBox) c).isSelected()){
+                    participantList.remove(findParticipant(((CheckBox) c).getText()));
+                }
+            }
+        }
+
+        if (howMuchField.getText() == null || howMuchField.getText().isEmpty()){
+            showErrorLabel("Please type in the amount!");
+            return;
+        }
+
         List<Debt> debts = createDebts(toEur(Double.parseDouble(howMuchField.getText()), currencySelector.getValue()), participantList);
 
+        //create the list of tags (God bless the creator of stream() :) )
+        List<Tag> tags = tagSelector.getChildren().stream().filter(n -> n.getClass() == CheckBox.class).map(n -> ((CheckBox) n).getText()).map(this::findTag).toList();
+
+        if (tags.isEmpty()){
+            showErrorLabel("Please select at least one tag or create one!");
+            return;
+        }
+
         //create the expense
-        Expense newExpense = new Expense("a", Double.parseDouble(howMuchField.getText()), whenField.getValue().atStartOfDay(), paidBy, event, debts, new ArrayList<>());
+        Expense newExpense = new Expense(paidBy.getName() + " paid for " + tags.get(0).getTag(), Double.parseDouble(howMuchField.getText()), whenField.getValue().atStartOfDay(), paidBy, event, debts, tags);
         for (Debt d : newExpense.getDebts()){
             d.setExpense(newExpense); //setup each debt's expense pointer
         }
@@ -135,5 +168,21 @@ public class AddExpenseCtrl implements Initializable {
 
     public void closeCreateTag(){
         createTagBox.setVisible(false);
+    }
+    public Participant findParticipant(String name){
+        Participant r = null;
+        for (Participant p : event.getParticipants()) if (p.getName().equals(name)) r = p;
+        return r;
+    }
+
+    public Tag findTag(String name){
+        Tag t = null;
+        for (Tag tag : event.getTags()) if (tag.getTag().equals(name)) t = tag;
+        return t;
+    }
+
+    public void showErrorLabel(String error){
+        errorLabel.setText(error);
+        errorLabel.setVisible(true);
     }
 }
