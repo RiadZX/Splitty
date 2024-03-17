abcaa11624c65f1f1a1487ad9c06057e202412c3
Andrei Ilinescu <a.ilinescu@student.tudelft.nl>
2024-03-17 23:40:14 +0100

    Made ParticipantController endpoints act in relation to the event Id

--- server/src/main/java/server/database/ParticipantRepository.java
+++ server/src/main/java/server/database/ParticipantRepository.java
@@ -2,9 +2,22 @@ package server.database;
 
 import commons.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import  org.springframework.data.jpa.repository.Modifying;
 
+import java.util.List;
 import java.util.UUID;
 
 
-public interface ParticipantRepository extends JpaRepository<Participant, UUID> {}
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+    @Query("SELECT p FROM Participant p WHERE p.event.id = :eventID")
+    List<Participant> getParticipantsFromEvent(UUID eventID);
+
+    @Query("SELECT p FROM Participant p WHERE p.event.id = :eventID AND  p.id= :participantID")
+    Participant findParticipantInEvent(UUID eventID, UUID participantID);
+
+    @Modifying
+    @Query("DELETE Participant p WHERE p.event.id = :eventID AND p.id= :participantID ")
+    void deleteParticipantFromEvent(UUID eventID, UUID participantID);
+}
 
