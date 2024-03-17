334318fa611c0a2e39c29cbeaa376ffe62077db1
Riad Zaid <r.zaid@student.tudelft.nl>
2024-03-17 12:04:18 +0100

    fixed unused imports :/

--- client/src/main/java/client/scenes/EventOverviewCtrl.java
+++ client/src/main/java/client/scenes/EventOverviewCtrl.java
@@ -8,11 +8,9 @@ import commons.Participant;
 import jakarta.ws.rs.WebApplicationException;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Modality;
 
 import java.net.URL;
 import java.util.List;
