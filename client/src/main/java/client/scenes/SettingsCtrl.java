bd4c5697d6328725821fe7944f4be57ee7480a18
Andrei Ilinescu <a.ilinescu@student.tudelft.nl>
2024-03-20 18:59:07 +0100

    Created Password Prompt

--- client/src/main/java/client/scenes/SettingsCtrl.java
+++ client/src/main/java/client/scenes/SettingsCtrl.java
@@ -1,19 +1,32 @@
 package client.scenes;
 
+import client.services.NotificationHelper;
 import client.services.NotificationService;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextInputDialog;
 
-public class SettingsCtrl {
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class SettingsCtrl implements Initializable {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
     private final NotificationService notificationService;
+
+    private TextInputDialog passwordPrompt;
 
     @Inject
     public SettingsCtrl(ServerUtils server, MainCtrl mainCtrl, NotificationService notificationService) {
         this.server = server;
         this.mainCtrl = mainCtrl;
         this.notificationService = notificationService;
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.prepareAdminLogin();
     }
 
     public void editProfileAction(){
@@ -24,5 +37,27 @@ public class SettingsCtrl {
     }
     public void backAction(){
         this.mainCtrl.showStartScene();
+    }
+
+    private void prepareAdminLogin(){
+        passwordPrompt=new TextInputDialog();
+        passwordPrompt.setTitle("Splitty: Admin Login");
+        passwordPrompt.setContentText("Admin password:");
+        passwordPrompt.setHeaderText("");
+        passwordPrompt.setGraphic(null);
+    }
+    public void adminAction(){
+        passwordPrompt.getEditor().clear(); // Clear Dialogue Input
+        Optional<String> result = passwordPrompt.showAndWait();
+        result.ifPresent(this::checkPassword); // if ok is pressed check password
+    }
+
+    private void checkPassword(String password){
+        if (password.isEmpty()){
+            NotificationHelper notificationHelper = new NotificationHelper();
+            notificationHelper.showError("Warning!", "Password cannot be blank...");
+            return;
+        }
+
     }
 }
