57de9e4ed8a62a42d2a603dd759ab2f0c96a32f0
Calin Andrei Radoi <C.A.Radoi@student.tudelft.nl>
2024-03-20 22:17:39 +0100

    Basic admin page done

--- client/src/main/java/client/scenes/AdminEventsCtrl.java
+++ client/src/main/java/client/scenes/AdminEventsCtrl.java
@@ -11,6 +11,8 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import java.net.URL;
@@ -23,7 +25,7 @@ public class AdminEventsCtrl implements Initializable {
     private final ServerUtils server;
 
     @FXML
-    private ListView<Event> myListView;
+    private ListView<BorderPane> myListView;
 
     private List<Event> events;
 
@@ -35,6 +37,7 @@ public class AdminEventsCtrl implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        /*
         myListView.setCellFactory(param -> new ListCell<Event>() {
             @Override
             protected void updateItem(Event item, boolean empty) {
@@ -42,6 +45,8 @@ public class AdminEventsCtrl implements Initializable {
                 if (empty || item == null) {
                     setText(null);
                 } else {
+                    BorderPane parent = new BorderPane();
+                    getChildren().add(parent);
                     setText(item.getName());
                     Image image = new Image("client/icons/bin.png");
                     ImageView remove = new ImageView();
@@ -50,11 +55,10 @@ public class AdminEventsCtrl implements Initializable {
                     remove.cursorProperty().set(Cursor.CLOSED_HAND);
                     remove.setFitHeight(12.0);
                     remove.setFitWidth(12.0);
-
-                    getChildren().add(remove);
                 }
             }
         });
+        */
     }
 
     private void removeEvent(Event item) {
@@ -65,12 +69,25 @@ public class AdminEventsCtrl implements Initializable {
     public void populateList() {
         myListView.getItems().clear();
         this.events = server.getEvents();
-        myListView.getItems().addAll(events);
-        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Event>() {
-            @Override
-            public void changed(ObservableValue<? extends Event> observable, Event oldValue, Event newValue) {
-                Event e = myListView.getSelectionModel().getSelectedItem();
-            }
-        });
+        List<BorderPane> contents = events.stream().map(e -> {
+            BorderPane bp = new BorderPane();
+            bp.setLeft(new Text(e.getName()));
+            Image image = new Image("client/icons/bin.png");
+            ImageView remove = new ImageView();
+            remove.setImage(image);
+            remove.setOnMouseClicked(x -> removeEvent(e));
+            remove.cursorProperty().set(Cursor.CLOSED_HAND);
+            remove.setFitHeight(12.0);
+            remove.setFitWidth(12.0);
+            bp.setRight(remove);
+            return bp;
+        }).toList();
+        myListView.getItems().addAll(contents);
+//        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Event>() {
+//            @Override
+//            public void changed(ObservableValue<? extends Event> observable, Event oldValue, Event newValue) {
+//                Event e = myListView.getSelectionModel().getSelectedItem();
+//            }
+//        });
     }
 }
