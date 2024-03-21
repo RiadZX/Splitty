02870f65d81417543c50d17d7800c56665659253
Riad Zaid <r.zaid@student.tudelft.nl>
2024-03-21 21:23:30 +0100

    Added service layer to the expense endpoints, changed the tests and added a boilerplate for testing the service layer

--- server/src/test/java/server/api/ExpenseControllerTest.java
+++ server/src/test/java/server/api/ExpenseControllerTest.java
@@ -10,7 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import server.database.ExpenseRepository;
 import server.service.ExpenseService;
 
 import java.util.List;
--- server/src/test/java/server/api/ExpenseServiceTest.java
+++ server/src/test/java/server/api/ExpenseServiceTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import server.database.ExpenseRepository;
 import server.service.ExpenseService;
 
