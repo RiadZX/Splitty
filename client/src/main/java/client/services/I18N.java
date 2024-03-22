7c8dbefe68b45d34f638ba3f10b6476e391601e9
Tijmen de Jong <t.b.dejong@student.tudelft.nl>
2024-03-22 17:35:26 +0100

    way to get strings now

--- client/src/main/java/client/services/I18N.java
+++ client/src/main/java/client/services/I18N.java
@@ -4,10 +4,8 @@ package client.services;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.text.MessageFormat;
+import java.util.*;
 
 public class I18N {
     private static final ObjectProperty<Locale> locale;
@@ -23,7 +21,13 @@ public class I18N {
      * @return List of all locales supported by the application.
      */
     public static List<Locale> getSupportedLocales() {
-        return new ArrayList<>(Arrays.asList(Locale.ENGLISH, new Locale.Builder().setLanguage("nl").setScript("Latn").setRegion("nl").build()));
+        return new ArrayList<>(Arrays.asList(
+                Locale.ENGLISH,
+                new Locale.Builder()
+                    .setLanguage("nl")
+                    .setScript("Latin")
+                    .setRegion("nl").build()
+        ));
     }
 
     /**
@@ -34,5 +38,34 @@ public class I18N {
     public static Locale getDefaultLocale() {
         Locale sysDefault = Locale.getDefault();
         return getSupportedLocales().contains(sysDefault) ? sysDefault : Locale.ENGLISH;
+    }
+
+    public static Locale getLocale() {
+        return locale.get();
+    }
+
+    public static void setLocale(Locale locale) {
+        localeProperty().set(locale);
+        Locale.setDefault(locale);
+    }
+
+    public static ObjectProperty<Locale> localeProperty() {
+        return locale;
+    }
+
+
+    /**
+     * gets the string with the given key from the resource bundle for the current locale and uses it as first argument
+     * to MessageFormat.format, passing in the optional args and returning the result.
+     *
+     * @param key
+     *         message key
+     * @param args
+     *         optional arguments for the message
+     * @return localized formatted string
+     */
+    public static String get(final String key, final Object... args) {
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", getLocale());
+        return MessageFormat.format(bundle.getString(key), args);
     }
 }
