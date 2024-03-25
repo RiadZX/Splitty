a572a92e6c9c468093e1a24839d95842655cdc53
Riad Zaid <r.zaid@student.tudelft.nl>
2024-03-25 21:28:33 +0100

    Fixed adding events, now uses an extra field to prevent infinite loading. just loads the event uuid.

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -32,11 +32,21 @@ public class Expense {
     @JoinColumn(name = "participant_id")
     @JsonBackReference ("participant-expenses")
     private Participant paidBy;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id")
     @JsonBackReference("event-expenses")
     private Event event;
+
+    public UUID getEventIdX() {
+        return eventIdX;
+    }
+
+    public void setEventIdX(UUID eventId) {
+        this.eventIdX = eventId;
+    }
+
+    //set by the server in the backend, when creating a new expense.
+    private UUID eventIdX;
 
     @ManyToMany
     @Expose
@@ -46,6 +56,7 @@ public class Expense {
     public String toString() {
         return "Expense{"
                 + "id=" + id
+                + ", event_id=" + eventIdX
                 + ", amount=" + amount
                 + ", date=" + date
                 + ", paidBy=" + paidBy
@@ -64,12 +75,13 @@ public class Expense {
     }
 
     public Expense(String title, double amount, Instant date,
-                   Participant paidBy, Event event, List<Debt> debts, List<Tag> tags) {
+                   Participant paidBy, Event event, UUID eventId, List<Debt> debts, List<Tag> tags) {
         this.title = title;
         this.amount = amount;
         this.date = date;
         this.paidBy = paidBy;
         this.event = event;
+        this.eventIdX = eventId;
         this.debts = debts;
         this.tags = tags;
     }
