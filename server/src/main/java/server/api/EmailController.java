6893264afee3a247a9da431d18ac82f485f3f10b
Calin Andrei Radoi <C.A.Radoi@student.tudelft.nl>
2024-03-25 16:02:33 +0100

    Added endpoints and service for sending emails

---
+++ server/src/main/java/server/api/EmailController.java
@@ -0,0 +1,31 @@
+package server.api;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.services.EmailSenderService;
+
+@RestController
+@RequestMapping("/api/mail")
+public class EmailController {
+    private final EmailSenderService emailSender;
+
+    @Autowired
+    public EmailController(EmailSenderService emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    @PostMapping(path = {"", "/"})
+    public ResponseEntity<Void> sendEmail(@RequestBody String contents) {
+        JsonObject body = JsonParser.parseString(contents).getAsJsonObject();
+        System.out.println(body);
+        System.out.println(body.get("toEmail"));
+
+        emailSender.sendEmail(body.get("toEmail").toString(), body.get("inviteCode").toString(), body.get("creator").toString());
+        return ResponseEntity.ok().build();
+    }
+}
