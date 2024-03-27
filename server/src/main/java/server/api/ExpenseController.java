6592eec992791c027faadcedb4db4c0cad4bd39f
Riad Zaid <r.zaid@student.tudelft.nl>
2024-03-27 15:56:39 +0100

    Merging

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -4,8 +4,8 @@ import commons.Expense;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import server.database.ExpenseRepository;
 import server.services.EventService;
+import server.services.ExpenseService;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,79 +13,70 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/events/{eventId}/expenses")
 public class ExpenseController {
-    private final ExpenseRepository repo;
+
+    //use service instead of repository
+    private final ExpenseService service;
     private final EventService eventService;
     @Autowired
-    public ExpenseController(ExpenseRepository repo, EventService eventService) {
-        this.repo = repo;
-        this.eventService=eventService;
+    public ExpenseController(ExpenseService service, EventService eventService) {
+        this.service = service;
+        this.eventService = eventService;
     }
 
-    /*
-        Get expenses for an event by event id
-     */
     @GetMapping(path = {"", "/"})
-    public  ResponseEntity<List<Expense>> getAll(@PathVariable String eventId) {
+    public  ResponseEntity<List<Expense>> getAll(@PathVariable("eventId") UUID eventId){
         try {
-            List<Expense> expenses = repo.findAll();
-            for (Expense expense : expenses) {
-                if (!expense.getEventIdX().equals(UUID.fromString(eventId))) {
-                    expenses.remove(expense);
+            List<Expense> e = service.getAllExpenses();
+            for (Expense expense : e) {
+                if (!expense.getEventIdX().equals(eventId)) {
+                    e.remove(expense);
                 }
             }
-            return ResponseEntity.ok(expenses);
+            return ResponseEntity.ok(e);
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
     }
 
     @PostMapping(path = {"", "/"})
-    public ResponseEntity<Expense> add(@PathVariable("eventId") String eventId, @RequestBody Expense expense) {
-        UUID eventUUID=UUID.fromString(eventId);
-        Expense saved = repo.save(expense);
-        if (repo.findById(saved.getId()).isPresent()) {
-            eventService.newEventLastActivity(eventUUID);
+    public ResponseEntity<Expense> add(@PathVariable("eventId") UUID eventId, @RequestBody Expense expense) {
+        Expense saved = service.addExpense(expense);
+        if (saved != null) {
+            eventService.newEventLastActivity(eventId);
             return ResponseEntity.ok(saved);
         }
         return ResponseEntity.internalServerError().build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Expense> getById(@PathVariable("eventId") String eventId, @PathVariable("id") UUID id) {
-        UUID eventUUID=UUID.fromString(eventId);
-        if (repo.findById(id).isPresent()) {
-            var saved=repo.findById(id).get();
-            eventService.newEventLastActivity(eventUUID);
-            return ResponseEntity.ok(saved);
+    public ResponseEntity<Expense> getById(@PathVariable("eventId") UUID eventId, @PathVariable("id") UUID id) {
+        Expense expense = service.getExpenseById(id);
+        if (expense != null) {
+            eventService.newEventLastActivity(eventId);
+            return ResponseEntity.ok(expense);
         }
         return ResponseEntity.badRequest().build();
     }
 
     // TODO Handle who can delete an expense and who can't
     @DeleteMapping("/{id}")
-    public ResponseEntity<Expense> remove(@PathVariable("eventId") String eventId, @PathVariable("id") UUID id) {
-        UUID eventUUID=UUID.fromString(eventId);
-        if (repo.findById(id).isPresent()) {
-            ResponseEntity<Expense> ret = ResponseEntity.ok(repo.findById(id).get());
-            repo.deleteById(id);
-            eventService.newEventLastActivity(eventUUID);
-            return ret;
+    public ResponseEntity<Expense> remove(@PathVariable("eventId") UUID eventId, @PathVariable("id") UUID id) {
+        Expense expense = service.deleteExpense(id);
+        if (expense != null) {
+            eventService.newEventLastActivity(eventId);
+            return ResponseEntity.ok(expense);
         }
         return ResponseEntity.badRequest().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Expense> update(@PathVariable("eventId") String eventId, @PathVariable("id") UUID id, @RequestBody Expense expense) {
-        UUID eventUUID=UUID.fromString(eventId);
-        if (repo.findById(id).isPresent()) {
-            repo.deleteById(id);
-            Expense saved = repo.save(expense);
-            if (repo.findById(saved.getId()).isPresent()) {
-                var saved2=repo.findById(saved.getId()).get();
-                this.eventService.newEventLastActivity(eventUUID);
-                return ResponseEntity.ok(saved2);
+    public ResponseEntity<Expense> update(@PathVariable("eventId") UUID eventId, @PathVariable("id") UUID id, @RequestBody Expense expense) {
+        if (id.equals(expense.getId())) {
+            Expense updated = service.updateExpense(expense);
+            if (updated != null) {
+                eventService.newEventLastActivity(eventId);
+                return ResponseEntity.ok(updated);
             }
-            return ResponseEntity.internalServerError().build();
         }
         return ResponseEntity.badRequest().build();
     }
