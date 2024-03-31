57384dd309d39d722e3fa07e609770616ffe0dbb
Tijmen de Jong <t.b.dejong@student.tudelft.nl>
2024-03-31 23:17:31 +0200

    seems to be workingish, but need to look at and fix something else before i can fully check

--- client/src/main/java/client/scenes/DebtResolveCtrl.java
+++ client/src/main/java/client/scenes/DebtResolveCtrl.java
@@ -3,19 +3,17 @@ package client.scenes;
 import client.services.NotificationService;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import commons.Debt;
 import commons.Event;
+import commons.Expense;
 import commons.Participant;
-import jakarta.ws.rs.WebApplicationException;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.TextFlow;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ResourceBundle;
 
 
@@ -25,6 +23,9 @@ public class DebtResolveCtrl implements Initializable {
 
     private Event event;
 
+    // the amounts that everyone is receiving
+    private HashMap<Participant, Amounts> amounts;
+
     @Inject
     public DebtResolveCtrl(ServerUtils server, MainCtrl mainCtrl, NotificationService notificationService) {
         this.server = server;
@@ -33,9 +34,74 @@ public class DebtResolveCtrl implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        this.amounts = new HashMap<>();
     }
 
     public void setEvent(Event event) {
         this.event = event;
+    }
+
+    public void refresh() {
+        amounts.clear();
+        List<Expense> expenses = event.getExpenses();
+
+        System.out.println(expenses);
+
+        for ( Expense e : expenses ) {
+            List<Debt> debts = e.getDebts();
+            amounts.merge(e.getPaidBy(),
+                    new Amounts(e.getAmount(), 0.),
+                    (start, end) -> new Amounts(
+                            start.giving() + end.giving(),
+                            start.getting() + end.getting())
+            );
+            for (Debt d : debts) {
+                amounts.merge(d.getParticipant(),
+                        new Amounts(0., d.getAmount()),
+                        (start, end) -> new Amounts(
+                                start.giving() + end.giving(),
+                                start.getting() + end.getting())
+                );
+            }
+        }
+
+        HashMap<Participant, Double> netAmounts = new HashMap<>();
+
+        for (Entry<Participant, Amounts> a : this.amounts.entrySet()) {
+            // calculate net spent
+            Double net = a.getValue().net();
+            netAmounts.put(a.getKey(), net);
+        }
+
+        Participant max = null;
+        // gotta get negative infinity
+        Double maxAmount = -1.0/0.0;
+
+        for (Entry<Participant, Double> a : netAmounts.entrySet()) {
+            if (a.getValue() > maxAmount) {
+                maxAmount = a.getValue();
+                max = a.getKey();
+            }
+        }
+
+        for (Entry<Participant, Double> a : netAmounts.entrySet()) {
+            netAmounts.put(a.getKey(), a.getValue() - maxAmount);
+        }
+
+        System.out.println("calculated");
+        System.out.println(netAmounts);
+    }
+}
+
+record Amounts(Double giving, Double getting) {
+    public Amounts add(Amounts one, Amounts two) {
+        return new Amounts(
+                one.giving + two.giving,
+                one.getting + two.getting
+        );
+    }
+
+    public Double net() {
+        return this.giving - this.getting;
     }
 }
