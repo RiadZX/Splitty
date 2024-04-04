6dadb913cd852bc22c61b13fffe124f4fb7c713b
Riad Zaid <r.zaid@student.tudelft.nl>
2024-04-04 17:28:47 +0200

    Tested pie chart with fake event data

--- client/src/main/java/client/scenes/StatisticsCtrl.java
+++ client/src/main/java/client/scenes/StatisticsCtrl.java
@@ -3,7 +3,10 @@ package client.scenes;
 import client.services.NotificationService;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
-import commons.*;
+import commons.Debt;
+import commons.Event;
+import commons.Expense;
+import commons.Tag;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -15,28 +18,35 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class StatisticsCtrl implements Initializable {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
     private final NotificationService notificationService;
-    @FXML public Label expenseLabel;
-    @FXML public TableColumn<StatsRow, String> tFrom;
-    @FXML public TableColumn<StatsRow, Double> tTo;
-    @FXML public TableColumn<StatsRow, Double> tAmount;
-    @FXML public TableView<StatsRow> tableView;
+    @FXML
+    public Label expenseLabel;
+    @FXML
+    public TableColumn<StatsRow, String> tFrom;
+    @FXML
+    public TableColumn<StatsRow, Double> tTo;
+    @FXML
+    public TableColumn<StatsRow, Double> tAmount;
+    @FXML
+    public TableColumn<StatsRow, String> tExpenseName;
+    @FXML
+    public TableView<StatsRow> tableView;
     private Event event;
 
-    @FXML private PieChart pieStats;
+    @FXML
+    private PieChart pieStats;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         tFrom.setCellValueFactory(new PropertyValueFactory<>("from"));
         tTo.setCellValueFactory(new PropertyValueFactory<>("to"));
         tAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        tExpenseName.setCellValueFactory(new PropertyValueFactory<>("expenseName"));
     }
 
     @Inject
@@ -45,83 +55,90 @@ public class StatisticsCtrl implements Initializable {
         this.mainCtrl = mainCtrl;
         this.notificationService = notificationService;
     }
+
     public void setEvent(Event event) {
         this.event = event;
     }
 
-    public void updatePieChart(){
+    public void updatePieChart() {
         pieStats.getData().clear();
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
         Map<String, Double> stats = new HashMap<>();
         //for each expense get the tags and add the amount to the corresponding tag
-        for (Expense e : event.getExpenses()){
-            for (Tag t : e.getTags()){
+        Event fakeEvent = getDummyPieChartData();
+        for (Expense e : fakeEvent.getExpenses()) {
+            for (Tag t : e.getTags()) {
                 stats.put(t.getTag(), stats.getOrDefault(t.getTag(), 0.0) + e.getAmount());
             }
         }
-        for (Map.Entry<String, Double> entry : stats.entrySet()){
+        for (Map.Entry<String, Double> entry : stats.entrySet()) {
             pieChartData.add(new PieChart.Data(entry.getKey(), entry.getValue()));
         }
         pieStats.setData(pieChartData);
     }
-    public void setParticipantStats(){
+
+    public void setParticipantStats() {
         tableView.getItems().clear();
         ObservableList<StatsRow> data = FXCollections.observableArrayList();
-        for (Expense e : event.getExpenses()){
-            for (Debt d : e.getDebts()){
-                if (d.isPaid()){
+        for (Expense e : event.getExpenses()) {
+            for (Debt d : e.getDebts()) {
+                if (d.isPaid()) {
                     continue;
                 }
-                data.add(new StatsRow(d.getParticipant().getName(), e.getPaidBy().getName(), d.getAmount()));
+                data.add(new StatsRow(d.getParticipant().getName(), e.getPaidBy().getName(), d.getAmount(), e.getTitle()));
             }
         }
         tableView.setItems(data);
     }
-//    public double calculateIncoming(Participant p){
-//        double incoming = 0;
-//        for (Expense e : event.getExpenses()){
-//            if (!e.getPaidBy().getId().equals(p.getId())){
-//                continue;
-//            }
-//            List<Debt> debts = e.getDebts();
-//            for (Debt d : debts) {
-//                if (d.isPaid()) {
-//                    continue;
-//                }
-//                incoming += d.getAmount();
-//            }
-//        }
-//        return incoming;
-//    }
-//
-//    public double calculateOutgoing(Participant p){
-//        double outgoing = 0;
-//        for (Expense e : event.getExpenses()){
-//            if (e.getPaidBy().getId().equals(p.getId())){
-//                continue;
-//            }
-//            List<Debt> debts = e.getDebts();
-//            for (Debt d : debts) {
-//                if (d.isPaid()) {
-//                    continue;
-//                }
-//                if (d.getParticipant().getId().equals(p.getId())){
-//                    outgoing+=d.getAmount();
-//                }
-//            }
-//        }
-//        return outgoing;
-//    }
 
-    public void setSumOfExpenses(){
+    /**
+     * Used while developing to generate dummy data for the pie chart
+     * @return a dummy event with expenses
+     */
+    public Event getDummyPieChartData() {
+        List<Tag> fakeTags = new ArrayList<>();
+        fakeTags.add(new Tag("Food"));
+        fakeTags.add(new Tag("Drinks"));
+        fakeTags.add(new Tag("Transport"));
+        fakeTags.add(new Tag("Accommodation"));
+        fakeTags.add(new Tag("Activities"));
+
+
+        Event e = new Event("Event ");
+
+        List<Tag> tags = new ArrayList<>();
+        //add tags randomly
+        int maxTags = new Random().nextInt(3);
+        for (int j = 0; j < maxTags; j++) {
+            tags.add(fakeTags.get(new Random().nextInt(fakeTags.size())));
+        }
+
+        for (int j = 0; j < 5; j++) {
+            Expense exp = new Expense(
+                    "Expense " + j,
+                    10,
+                    new Date().toInstant(),
+                    null,
+                    e,
+                    new ArrayList<>(),
+                    tags
+            );
+            exp.addTag(fakeTags.get(j));
+            e.addExpense(exp);
+        }
+        return e;
+
+    }
+
+    public void setSumOfExpenses() {
         double sum = 0;
-        for (Expense e : event.getExpenses()){
+        for (Expense e : event.getExpenses()) {
             sum += e.getAmount();
         }
         expenseLabel.setText("Total expenses: " + sum);
     }
 
-    public void back(){
+    public void back() {
         mainCtrl.showEventOverviewScene(event);
     }
 
@@ -130,24 +147,34 @@ public class StatisticsCtrl implements Initializable {
         setSumOfExpenses();
         setParticipantStats();
     }
+
     public class StatsRow {
         private final String from;
         private final String to;
         private final double amount;
+        private final String expenseName;
 
-        public StatsRow(String name, String to, double amount) {
+        public StatsRow(String name, String to, double amount, String expenseName) {
             this.from = name;
             this.to = to;
-            this.amount=amount;
+            this.amount = amount;
+            this.expenseName = expenseName;
         }
-        public String getFrom(){
+
+        public String getFrom() {
             return from;
         }
-        public String getTo(){
+
+        public String getTo() {
             return to;
         }
-        public double getAmount(){
+
+        public double getAmount() {
             return amount;
+        }
+
+        public String getExpenseName() {
+            return expenseName;
         }
     }
 }
