63b37cbc6e7b7c529c87a957b7c5cd0dc338bea0
Tijmen de Jong <t.b.dejong@student.tudelft.nl>
2024-04-12 13:30:44 +0200

    remove more trailing spaces

--- client/src/main/java/client/scenes/EventOverviewCtrl.java
+++ client/src/main/java/client/scenes/EventOverviewCtrl.java
@@ -200,7 +200,6 @@ public class EventOverviewCtrl implements Initializable {
         mainCtrl.showSettleDebt(this.event);
     }
 
-
     public void reassignParticipants(List<Participant> participantList){
         System.out.println(participantList.stream().map(Participant::getName).toList());
         Platform.runLater(() -> {
