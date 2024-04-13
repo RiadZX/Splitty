eba83efae5088db5086a744fc10ef4dbf712b80a
Calin Andrei Radoi <C.A.Radoi@student.tudelft.nl>
2024-04-13 18:10:19 +0200

    Fixed tests

--- commons/src/main/java/commons/Participant.java
+++ commons/src/main/java/commons/Participant.java
@@ -47,7 +47,6 @@ public class Participant {
 
     public Participant() {
         this.paidFor = new ArrayList<>();
-        this.debts = new ArrayList<>();
     }
 
     public Participant(String name) {
